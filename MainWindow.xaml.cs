a31db3e534ff2515ed69a4077c62f922813571fb
StellarLay <stellarlay@yandex.ru>
2020-01-26 19:21:00 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AirportService
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // placeholder button
+        private void loginBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if(loginText.Text == "Имя пользователя")
+            {
+                loginText.Text = "";
+            }
+        }
+
+        // placeholder button
+        private void loginText_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (loginText.Text == "")
+            {
+                loginText.Text = "Имя пользователя";
+            }
+        }
+
+        // placeholder button
+        private void passText_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (passText.Text == "Пароль")
+            {
+                passText.Text = "";
+            }
+        }
+
+        // placeholder button
+        private void passText_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (passText.Text == "")
+            {
+                passText.Text = "Пароль";
+            }
+        }
+
+        // перетаскивание окна зажатием ЛКМ
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ UserControlWindows.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AirportService
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControlWindows.xaml
+    /// </summary>
+    public partial class UserControlWindows : UserControl
+    {
+        public UserControlWindows()
+        {
+            InitializeComponent();
+        }
+
+        // Нажатие на крестик для выхода из app
+        private void exitBtn_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if(MessageBox.Show("Вы действительно хотите выйти?",
+                "Сообщение",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                Window.GetWindow(this).Close();
+            }
+        }
+        // Кнопка "Свернуть"
+        private void minBtn_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+    }
+}
